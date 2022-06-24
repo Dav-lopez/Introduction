c078e35f8f1bf2f67ba3231e84afd01e006f9add
Dav-lopez <78981910+Dav-lopez@users.noreply.github.com>
2022-06-24 19:32:02 +0100

    2

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const Height = 5.9;
 const country = 'Nigeria';
 
 
-alert(`Let give you a brief introduction of myself \n ${br} My Name is ${Name} \n I am ${Height}ft tall \n  My country is ${country}`)
+alert(`Let give you a brief introduction of myself \n\n My Name is ${Name} \n I am ${Height}ft tall \n My country is ${country}`)
