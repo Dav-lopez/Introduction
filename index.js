44b2aa853316e343766ec85ccbc862c6abce990e
Dav-lopez <78981910+Dav-lopez@users.noreply.github.com>
2022-06-24 19:29:16 +0100

    js folder

---
+++ index.js
@@ -0,0 +1,8 @@
+const Name = 'Lopez Jesustoba';
+
+const Height = 5.9;
+
+const country = 'Nigeria';
+
+
+alert(`Let give you a brief introduction of myself \n ${br} My Name is ${Name} \n I am ${Height}ft tall \n  My country is ${country}`)
